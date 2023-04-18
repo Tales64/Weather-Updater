9810d7a50db5b57104cc1266708923f4cc6572fa
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2023-04-17 17:07:34 -0700

    fixed the icons issues

--- script.js
+++ script.js
@@ -1,41 +1,52 @@
 var APIKey = "cbf81724fcd599755422dc8888dab750";
-var GOOGLEKEY = "AIzaSyBaOvWcc70LEcV3x1JjDigzWfouxsb03yM"
+var GOOGLEKEY = "AIzaSyBaOvWcc70LEcV3x1JjDigzWfouxsb03yM";
 var previousLocations = [];
-
+var iconChoices = [
+  { id: " 01d", image: "01d@2x.png" },
+  { id: "01d", image: "./Assets/01n@2x.png" },
+  { id: "02d", image: "./Assets/02d@2x.png" },
+  { id: "02n", image: "./Assets/02n@2x.png" },
+  { id: "03d", image: "./Assets/03d@2x.png" },
+  { id: "03n", image: "./Assets/03n@2x.png" },
+  { id: "04d", image: "./Assets/04d@2x.png" },
+  { id: "04n", image: "./Assets/04n@2x.png" },
+  { id: "09d", image: "./Assets/09d@2x.png" },
+  { id: "09n", image: "./Assets/09n@2x.png" },
+  { id: "10d", image: "./Assets/10d@2x.png" },
+  { id: "10n", image: "./Assets/10n@2x.png" },
+  { id: "11d", image: "./Assets/11d@2x.png" },
+  { id: "11n", image: "./Assets/11n@2x.png" },
+  { id: "13d", image: "./Assets/13d@2x.png" },
+  { id: "50d", image: "./Assets/50d@2x.png" },
+];
 // // create connections to html for placing text
 
 // // select the dropdown area
-var dropDownUl = $(`#dropdown-container`)
-var previousLocationsEl = $(`#dropdown-container`)
+var dropDownUl = $(`#dropdown-container`);
+var previousLocationsEl = $(`#dropdown-container`);
 
 // // grab all locations
 
-
 // // main weather area
-var cityImage= $(`#city-image`)
-var weatherImage= $(`#weather-image`)
-var weatherHeader = $(`#weather-header`)
-var weatherInfo = $(`#weather-description`)
-var weatherTemp= $(`#weather-temp`)
-var weatherWind= $(`#weather-wind`)
-var weatherHumidity= $(`#weather-humidity`)
-
-
-
+var cityImage = $(`#city-image`);
+var weatherImage = $(`#weather-image`);
+var weatherHeader = $(`#weather-header`);
+var weatherInfo = $(`#weather-description`);
+var weatherTemp = $(`#weather-temp`);
+var weatherWind = $(`#weather-wind`);
+var weatherHumidity = $(`#weather-humidity`);
 
 // select the form for inputs
-var formArea = $(`#location-form`)
-var formInput = $(`#form-input`)
-var formButton = $(`#form-btns`)
-var forecastHeader =$("#forecast1-header")
+var formArea = $(`#location-form`);
+var formInput = $(`#form-input`);
+var formButton = $(`#form-btns`);
+var forecastHeader = $("#forecast1-header");
 
 // Submit event on the form
 
 // // create text to fill those areas
-
 // // main weather area
 // // .text();
-var Town = formInput.val();
 
 var getStorage = () => {
   var storedLocations = JSON.parse(localStorage.getItem("cities"));
@@ -45,21 +56,21 @@ var getStorage = () => {
       createCityButton(city);
     }
   }
-}
+};
 
-formButton.on("click", function(event) {
-  event.preventDefault()
+formButton.on("click", function (event) {
+  event.preventDefault();
   
   // forecastHeader.text(`bland farts dont rise`);
   
-  console.log('First Name:');
-  console.log('Last Name:', formInput.val());
+  console.log("First Name:");
+  console.log("Last Name:", formInput.val());
   
   // formSubmit()
   var cityName = formInput.val();
-  console.log(cityName)
+  console.log(cityName);
   if (cityName) {
-    if(!previousLocations.includes(cityName)) {
+    if (!previousLocations.includes(cityName)) {
       previousLocations.push(cityName);
       createCityButton(cityName);
       addToStorage(previousLocations);
@@ -69,45 +80,45 @@ formButton.on("click", function(event) {
     
     // formInput.val() = '';
   } else {
-    alert('Please enter a city.');
-}
+    alert("Please enter a city.");
+  }
 });
 
-
-
 var createCityButton = (city) => {
-  var cityButton = $(`<button type="button" class="btn btn-primary">${city}</button>`);
-  cityButton.addClass('btn');
-  cityButton.attr("city-name", city);
-  cityButton.text(city);
-  dropDownUl.append(cityButton);
-}
-
-var addToStorage = (previousLocations) => {
-  var citiesToStore = JSON.stringify(previousLocations);
+  var cityButton = $(
+    `<button type="button" class="btn btn-primary">${city}</button>`
+    );
+    cityButton.addClass("btn");
+    cityButton.attr("city-name", city);
+    cityButton.text(city);
+    dropDownUl.append(cityButton);
+  };
+  
+  var addToStorage = (previousLocations) => {
+    var citiesToStore = JSON.stringify(previousLocations);
   localStorage.setItem("cities", citiesToStore);
-}
+};
 
 // // Create event handler
 
 var getWeather = (city) => {
   var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-  console.log(apiURL)
+  console.log(apiURL);
   fetch(apiURL)
   .then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
         displayData(data, location);
-        console.log(data)
+        console.log(data);
       });
     } else {
       alert(`Error: ${response.statusText}`);
     }
   })
   .catch(function (error) {
-    alert('Unable to connect to current weather API.');
+    alert("Unable to connect to current weather API.");
   });
-}
+};
 
 var getForecast = (city) => {
   // cityImage.attr( `src`, `https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&photoreference=${city}&key=${GOOGLEKEY}`);
@@ -124,99 +135,111 @@ var getForecast = (city) => {
     }
   })
   .catch(function (error) {
-    alert('Unable to connect to forecast API.');
+    alert("Unable to connect to forecast API.");
   });
-  console.log(forecastApiUrl)
+  console.log(forecastApiUrl);
 };
-
-
-var displayData = (weatherData, locationName) => {
-  console.log(weatherData);
-  var date = new Date();
-  var year = date.getFullYear();
-  var month = date.getMonth();
-  var day = date.getDate();
-  var tempKelvin = weatherData.main.temp;
-  var tempFahrenheit = 1.8 * (tempKelvin - 273) + 32;
-  var weatherDescriptions = weatherData.weather[0].description;
-  var weatherIcon = weatherData.weather[0].icon;
-  var windSpeedMPS = weatherData.wind.speed;
-  var windSpeedMPH = windSpeedMPS * 2.23694;
-  var humidityPercentage = weatherData.main.humidity;
+var buttonClick = (event) => {
+    var cityName = event.target.getAttribute("city-name");
+    
+    if (cityName) {
+      getWeather(cityName);
+      getForecast(cityName);
+    }
+  };
   
-  weatherHeader.text(`${locationName}( ${month} / ${day} / ${year} )`);
-  weatherImage.attr(`src`, `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`);
+  var displayData = (weatherData, cityName) => {
+    console.log(weatherData);
+    var Town = formInput.val();
+    var todaysDate = new Date();
+    var year = todaysDate.getFullYear();
+    var month = todaysDate.getMonth();
+    var day = todaysDate.getDate();
+    var tempKelvin = weatherData.main.temp;
+    var tempFahrenheit = 1.8 * (tempKelvin - 273) + 32;
+    var weatherDescriptions = weatherData.weather[0].description;
+    var weatherIcon = weatherData.weather[0].icon;
+    var windSpeedMPS = weatherData.wind.speed;
+    var windSpeedMPH = windSpeedMPS * 2.23694;
+    var humidityPercentage = weatherData.main.humidity;
+  
+    weatherHeader.text(`${Town || cityName.value}( ${month + 1} / ${day} / ${year} )`);
+    console.log(cityName);
+  weatherImage.attr(
+    `src`,
+    `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+    );
+    console.log(cityName.value);
   // weatherIcon.classList.remove("hidden");
   // weatherIcon.classList.add("show");
   weatherInfo.text(weatherData.weather.description);
-  weatherTemp.text(`Temp:${tempFahrenheit.toFixed(2)} deg F`);;
-weatherWind.text(`Wind:${windSpeedMPH.toFixed(2)} MPH`);
-weatherHumidity.text(`Humidity:${humidityPercentage} %`);
-}
+  weatherTemp.text(`Temp:${tempFahrenheit.toFixed(2)} deg F`);
+  weatherWind.text(`Wind:${windSpeedMPH.toFixed(2)} MPH`);
+  weatherHumidity.text(`Humidity:${humidityPercentage} %`);
+};
 
 var displayForecast = (forecastData) => {
   console.log(forecastData);
   var today = new Date();
+
   for (let i = 0; i < 5; i++) {
-    for (let i = 0; i < 5; i++) { 
-      idValue = i + 1
-      
-      
-      // forecast area
-      var forecastImage= $(`#forecast${idValue}-image`)
-      var forecastHeader = $(`#forecast${idValue}-header`)
-      var forecastDescription = $(`#forecast${idValue}-description`)
-      var forecastTemp= $(`#forecast${idValue}-temp`)
-      var forecastWind= $(`#forecast${idValue}-wind`)
-      var forecastHumidity= $(`#forecast${idValue}-humidity`)
-      // forecast area
-      const forecastDay = new Date(today);
+    idValue = i + 1;
+
+    // forecast area
+    // var forecastImage= $(`#forecast${idValue}-image`)
+    var forecastHeader = $(`#forecast${idValue}-header`);
+    var forecastDescription = $(`#forecast${idValue}-description`);
+    var forecastTemp = $(`#forecast${idValue}-temp`);
+    var forecastWind = $(`#forecast${idValue}-wind`);
+    var forecastHumidity = $(`#forecast${idValue}-humidity`);
+    // forecast area
+    const forecastDay = new Date(today);
     forecastDay.setDate(forecastDay.getDate() + i + 1);
     var year = forecastDay.getFullYear();
     var month = forecastDay.getMonth();
     var day = forecastDay.getDate();
-    var tempKelvin = forecastData.list[(i * 7) + 5].main.temp;
+    var tempKelvin = forecastData.list[idValue].main.temp;
     var tempFahrenheit = 1.8 * (tempKelvin - 273) + 32;
-    var forecastIcon = forecastData.list[(i * 7) + 5].weather[0].icon;
-    var weatherDescription = forecastData.list[(i * 7) + 5].weather[0].description;
-    var windSpeedMPS = forecastData.list[(i * 7) + 5].wind.speed;
-    var windSpeedMPH = windSpeedMPS * 2.23694; 
-    var humidityPercentage = forecastData.list[(i * 7) + 5].main.humidity;
-      
-    forecastImage.text(`<img src=http://openweathermap.org/img/wn/${forecastIcon}@2x.png>`);
-      forecastHeader.text(day);
-      forecastDescription.text(weatherDescription);
-      forecastTemp.text(`Temp:${tempFahrenheit.toFixed(2)} deg F`);
-      forecastWind.text(`Wind:${windSpeedMPH.toFixed(2)} MPH`);
-      forecastHumidity.text(`Humidity:${humidityPercentage} %`);
-      
-      console.log(i)
-      
+    var forecastIcon = forecastData.list[idValue].weather[0].icon;
+    var weatherDescription = forecastData.list[idValue].weather[0].description;
+    var windSpeedMPS = forecastData.list[idValue].wind.speed;
+    var windSpeedMPH = windSpeedMPS * 2.23694;
+    var humidityPercentage = forecastData.list[idValue].main.humidity;
+    // forecastImage.text(`http://openweathermap.org/img/wn/${forecastIcon}@2x.png`);
+    forecastHeader.text(day);
+    forecastDescription.text(weatherDescription);
+    forecastTemp.text(`Temp:${tempFahrenheit.toFixed(2)} deg F`);
+    forecastWind.text(`Wind:${windSpeedMPH.toFixed(2)} MPH`);
+    forecastHumidity.text(`Humidity:${humidityPercentage} %`);
+    
+    campFire(forecastIcon);
+    // console.log(forecastImage)
+  }
+};
+// place weather icons at the appropriate ids
+function campFire(forecastIcon) {
+  for (let x = 0; x < iconChoices.length; x++) {
+    if (forecastIcon == iconChoices[x].id) {
+      forecastIcon = iconChoices[x].image;
+      var displayedIcon = forecastIcon;
+      // console.log(displayData)
+      $(`#forecast${idValue}-icon`).attr("src", displayedIcon);
+      console.log(displayedIcon);
     }
   }
 };
 
-var buttonClick = (event) => {
-  var cityName = event.target.getAttribute('city-name');
 
-  if(cityName) {
-    getWeather(cityName);
-    getForecast(cityName);
-  }
-}
-
-previousLocationsEl.on('click', buttonClick);
+previousLocationsEl.on("click", buttonClick);
 getStorage();
 // var buttonClickHandler = (event) => {
 //   var locationName = event.target.getAttribute('city-name');
-  
+
 //   if(locationName) {
 //     getWeather(locationName);
 //     getForecast(locationName);
 //   }
 // }
-
-
 
 // previousLocationsEl.on('click', buttonClickHandler);
 // grab api data
